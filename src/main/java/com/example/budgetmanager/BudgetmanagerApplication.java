c06ed29b3896f9b9ea5839747ba8a89070252ba2
Kyoseva7 <albena.kyoseva7@gmail.com>
2025-04-26 23:57:22 +0300

    Update BudgetmanagerApplication.java

--- src/main/java/com/example/budgetmanager/BudgetmanagerApplication.java
+++ src/main/java/com/example/budgetmanager/BudgetmanagerApplication.java
@@ -8,6 +8,6 @@ public class BudgetmanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetmanagerApplication.class, args);
+		System.out.println("test");
 	}
-
 }
